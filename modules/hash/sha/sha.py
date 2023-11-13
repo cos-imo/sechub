2c7507d35a322c9e0b248c9a22d3da9174009da4
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-11-13 15:12:36 +0100

    [~]code: Adding SHA1, SHA224, SHA384, and SHA256 support

--- modules/hash/sha/sha.py
+++ modules/hash/sha/sha.py
@@ -1,14 +1,76 @@
-import argparse
+import hashlib 
 
-class Parser:
+class sha_hasher:
     def __init__(self):
-        self.parser = argparse.ArgumentParser(usage='python3 main.py [OPTIONS]', add_help=False, description='Python cybersecurity toolbox')
-        self.parse_arguments()
+        pass
 
-    def parse_arguments(self):
+    def hexdigest_sha256_hash(self, text_to_hash):
+        """
+        Calculates SHA256 hash of text_to_hash and returns it 
+        """
 
-        self.parser.add_argument('-h','--help', action='help', help="Affiche ce message d'aide")
+        return (hashlib.sha256(text_to_hash.encode)).hexdigest()
 
-        self.args = self.parser.parse_args()
+    def byteequivalent_sha256_hash(self, text_to_hash):
+        """
+        Calculates SHA256 byte equivalent of text_to_hash and returns it
+        """
 
+        return hashlib.sha256(text_to_hash.encode())
+
+    def hexdigest_sha384_hash(self, text_to_hash):
+        """
+        Calculates SHA384 hash of text_to_hash and returns it 
+        """
+
+        return (hashlib.sha384(text_to_hash.encode)).hexdigest()
+
+    def byteequivalent_sha384_hash(self, text_to_hash):
+        """
+        Calculates SHA384 byte equivalent of text_to_hash and returns it
+        """
+
+        return hashlib.sha384(text_to_hash.encode())
+
+    def hexdigest_sha224_hash(self, text_to_hash):
+        """
+        Calculates SHA224 hash of text_to_hash and returns it 
+        """
+
+        return (hashlib.sha224(text_to_hash.encode)).hexdigest()
+
+    def byteequivalent_sha224_hash(self, text_to_hash):
+        """
+        Calculates SHA224 byte equivalent of text_to_hash and returns it
+        """
+
+        return hashlib.sha224(text_to_hash.encode())
+
+    def hexdigest_sha512_hash(self, text_to_hash):
+        """
+        Calculates SHA512 hash of text_to_hash and returns it 
+        """
+
+        return (hashlib.sha512(text_to_hash.encode)).hexdigest()
+
+    def byteequivalent_sha512_hash(self, text_to_hash):
+        """
+        Calculates SHA512 byte equivalent of text_to_hash and returns it
+        """
+
+        return hashlib.sha512(text_to_hash.encode())
+
+    def hexdigest_sha1_hash(self, text_to_hash):
+        """
+        Calculates SHA1 hash of text_to_hash and returns it 
+        """
+
+        return (hashlib.sha1(text_to_hash.encode)).hexdigest()
+
+    def byteequivalent_sha1_hash(self, text_to_hash):
+        """
+        Calculates SHA1 byte equivalent of text_to_hash and returns it
+        """
+
+        return hashlib.sha1(text_to_hash.encode())
 
